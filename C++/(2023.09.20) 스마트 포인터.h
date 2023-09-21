68017281ffa07c71abc6aae2dc8969e2ec91461c
Yunbinnn <139340918+Yunbinnn@users.noreply.github.com>
2023-09-21 19:47:38 +0900

    (2023.09.21) 템플릿

---
+++ C++/(2023.09.20) 스마트 포인터.h
@@ -0,0 +1,106 @@
+#pragma once
+#include <iostream>
+#include <memory>
+using namespace std;
+
+#pragma region RAII 패턴
+// 자원의 안전한 사용을 위해
+// 객체가 쓰이는 범위를 벗어나게 되면
+// 자원을 해제해주는 기법입니다.
+
+class File
+{
+public:
+	File()
+	{
+		cout << "Create File" << endl;
+	}
+
+	~File()
+	{
+		cout << "Delete File" << endl;
+	}
+};
+#pragma endregion
+
+class Player
+{
+private:
+	int health = 0;
+
+#pragma region weak 포인터
+	// 자신이 참조하고 있는 원시 포인터의 강한 참조 카운트가 0이 되면
+	// expired 라는 상태가 되는데, 이는 원시 포인터의 참조 카운트가 0이
+	// 되어 메모리가 해제되었으므로, 이 원시 포인터를 소유한 weak 포인터를
+	// 유효하지 않다고 판단하여 해제하는 포인터입니다.
+
+	weak_ptr<Player> partner;
+#pragma endregion
+
+public:
+	Player();
+	~Player();
+
+	void SetPartner( shared_ptr<Player> partner );
+};
+
+Player::Player()
+{
+	cout << "Create Player" << endl;
+}
+
+Player::~Player()
+{
+	cout << "Delete Player" << endl;
+}
+
+void Player::SetPartner( shared_ptr<Player> partner )
+{
+	this->partner = partner;
+}
+
+int main()
+{
+#pragma region unique 포인터
+	// 단 하나의 객체만 가리킬 수 있는
+	// 스마트 포인터입니다.
+
+	/*unique_ptr<File> uptr1( new File );
+
+	cout << "uptr1의 값 : " << uptr1 << endl;
+
+	unique_ptr<File> uptr2 = make_unique<File>();
+
+	cout << "uptr2의 값 : " << uptr2 << endl;*/
+
+	// 하나의 unique 포인터는 하나의 객체만 가질 수 있습니다.
+	// 하지만, 객체에 대 한 소유권을 이전하는 것은 가능합니다.
+	/*unique_ptr<File> uptr3 = move( uptr2 );
+
+	cout << "uptr3의 값 : " << uptr3 << endl;*/
+
+#pragma endregion
+
+#pragma region shared 포인터
+	// 하나의 객체에 여러개의 포인터가 공유할 수 있으며,
+	// 공유할 때마다 참조 카운트를 이용해서 메모리를 
+	// 관리하는 스마트 포인터입니다.
+
+	/*shared_ptr<File> filePtr1( new File );
+
+	cout << filePtr1.use_count() << endl;
+
+	shared_ptr<File> filePtr2 = filePtr1;
+
+	cout << filePtr1.use_count() << endl;*/
+
+	shared_ptr<Player> player1 = make_shared <Player>();
+	shared_ptr<Player> player2 = make_shared <Player>();
+
+	player1->SetPartner( player2 );
+	player2->SetPartner( player1 );
+
+#pragma endregion
+
+	return 0;
+}
--- C++/C++.cpp
+++ C++/C++.cpp
@@ -1,103 +1,42 @@
 ﻿#include <iostream>
-#include <memory>
+#include "LinkedList.h"
 using namespace std;
 
-#pragma region RAII 패턴
-	// 자원의 안전한 사용을 위해
-	// 객체가 쓰이는 범위를 벗어나게 되면
-	// 자원을 해제해주는 기법입니다.
+#pragma region 템플릿
+	// 데이터 형식에 의존하지 않고, 하나의 값이
+	// 여러 다 른 데이터 타입들을 가질 수 있는
+	// 기술에 중점을 두어 재사용성을 높일 수 있는 기능입니다.
 
-class File
+template<typename T>
+void Add( T value1, T value2 )
 {
-public:
-	File() 
-	{
-		cout << "Create File" << endl;
-	}
-
-	~File()
-	{
-		cout << "Delete File" << endl;
-	}
-};
-#pragma endregion
-
-class Player
-{
-private:
-	int health = 0;
-
-#pragma region weak 포인터
-	// 자신이 참조하고 있는 원시 포인터의 강한 참조 카운트가 0이 되면
-	// expired 라는 상태가 되는데, 이는 원시 포인터의 참조 카운트가 0이
-	// 되어 메모리가 해제되었으므로, 이 원시 포인터를 소유한 weak 포인터를
-	// 유효하지 않다고 판단하여 해제하는 포인터입니다.
-
-	weak_ptr<Player> partner;
-#pragma endregion
-
-public:
-	Player();
-	~Player();
-
-	void SetPartner( shared_ptr<Player> partner );
-};
-
-Player::Player()
-{
-	cout << "Create Player" << endl;
+	cout << value1 + value2 << endl;
 }
 
-Player::~Player()
+template<>
+void Add( char value1, char value2 )
 {
-	cout << "Delete Player" << endl;
+	cout << value1 << " : " << value2 << endl;
 }
 
-void Player::SetPartner( shared_ptr<Player> partner )
+template<typename T1, typename T2>
+void Add( T1 value1, T2 value2 )
 {
-	this->partner = partner;
+	cout << value1 + value2 << endl;
 }
+	
+#pragma endregion  
 
 int main()
 {
-#pragma region unique 포인터
-	// 단 하나의 객체만 가리킬 수 있는
-	// 스마트 포인터입니다.
+#pragma region 템플릿
 
-	/*unique_ptr<File> uptr1( new File );
+	/*Add( 'A', 'B');
+	Add( 10, 15.75 );
+	Add( 5.75f, 4.85f );*/
 
-	cout << "uptr1의 값 : " << uptr1 << endl;
-
-	unique_ptr<File> uptr2 = make_unique<File>();
-
-	cout << "uptr2의 값 : " << uptr2 << endl;*/
-
-	// 하나의 unique 포인터는 하나의 객체만 가질 수 있습니다.
-	// 하지만, 객체에 대 한 소유권을 이전하는 것은 가능합니다.
-	/*unique_ptr<File> uptr3 = move( uptr2 );
-
-	cout << "uptr3의 값 : " << uptr3 << endl;*/
-
-#pragma endregion
-
-#pragma region shared 포인터
-	// 하나의 객체에 여러개의 포인터가 공유할 수 있으며,
-	// 공유할 때마다 참조 카운트를 이용해서 메모리를 
-	// 관리하는 스마트 포인터입니다.
-
-	/*shared_ptr<File> filePtr1( new File );
-
-	cout << filePtr1.use_count() << endl;
-
-	shared_ptr<File> filePtr2 = filePtr1;
-
-	cout << filePtr1.use_count() << endl;*/
-
-	shared_ptr<Player> player1 = make_shared <Player>();
-	shared_ptr<Player> player2 = make_shared <Player>();
-
-	player1->SetPartner( player2 );
-	player2->SetPartner( player1 );
+	LinkedList<int> linkedlist{};
+	linkedlist.Push( 10 );
 
 #pragma endregion
 
